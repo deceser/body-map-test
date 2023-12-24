b0b260e45ed900f359592217c8abfb35e380bbbe
deceser <aufhaummtobsc@gmail.com>
2023-12-24 21:37:45 +0200

    upd for mobile media-screen

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Устанавливаем порт 3000
+    host: "192.168.1.3", // Устанавливаем желаемый хост
+  },
+});
